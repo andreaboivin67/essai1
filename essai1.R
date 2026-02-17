630975cd1e7b1b4cb7e696605eba277fe175c396
andreaboivin67 <andrea.boivin@usherbrooke.ca>
2026-02-17 10:02:42 -0500

    j'ai ajouté encore de quoi

--- essai1.R
+++ essai1.R
@@ -3,3 +3,5 @@ voici ma première ligne que jecris trololol
 jajoute encore autre chose pour confirmer la visualisation des versions sur GitHub en ligne
 
 modif qui est sensée ne pas fonctionner car confusion
+
+exemple saida
